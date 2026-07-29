4888d35892389f86029d0e1ab4b83104b7e4db72
vanshtarpe01 <vanshvtarpe@gmail.com>
2026-07-29 19:36:19 +0530

    Basics of array done

--- Arrays/BinarySearch.java
+++ Arrays/BinarySearch.java
@@ -16,9 +16,57 @@ public class BinarySearch {
         }
     }
 
-    // public static void binarySearch(int[] arr, int target)
+    public static void binarySearch(int[] arr, int target){
+        int low =0;
+        int high = arr.length-1;
+
+        while(low<=high){
+            int mid = low+(low+high)/2;
+
+            if(arr[mid]==target){
+                System.out.println("Element is pesent at index : "+ mid);
+                return;
+            }
+
+            if(arr[mid]<target){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+    }
+
+    public static void binarySearchRecursion(int[] arr, int target, int low, int high){
+        int mid = low+(low+high)/2;
+
+        if(arr[mid]== target){
+            System.out.println("Element is present at index: "+ mid);
+            return;
+        }else if(arr[mid]<target){
+            binarySearchRecursion(arr, target, mid+1, high);
+        }else{
+            binarySearchRecursion(arr, target, low, mid-1);
+        }
+    }
 
     public static void main(String[] args){
+        int size;
+        System.out.print("Enter a size of an array :- ");
+        size = sc.nextInt();
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of an array :- ");
+        gettingArrayElements(arr);
+
+        System.out.print("Printing Elements of an Array:- ");
+        tarversing(arr);
+
+        System.out.println();
+
+        int target;
+        System.out.print("Enter a elemnt that you want to search in array :- ");
+        target = sc.nextInt();
+        binarySearch(arr, target);
+        binarySearchRecursion(arr, target, 0, arr.length-1);
 
     }
 }
